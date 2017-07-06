f09b5e81bfe18a836de2135573029a55f8b82f37
Andrey Luiz <andrey.luiz@datarisk.io>
2017-07-06 17:24:03 -0300

    Create an ad info component.

---
+++ src/components/Advertisement/components/Info/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+
+export default () => (
+  <div styleName="info">
+    <div styleName="body">
+      <h2>Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa</h2>
+      <p>35764 Sinn / Fleisbach</p>
+    </div>
+    <div styleName="footer">
+      <div styleName="price">
+        <h2>1.954 €</h2>
+      </div>
+      <div styleName="size">
+        <div styleName="bedrooms">
+          <span>3 Zimmer</span>
+        </div>
+        <div styleName="metres">
+          <span>ab 35 m²</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
--- src/components/Advertisement/index.jsx
+++ src/components/Advertisement/index.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import TypeBadge from './components/TypeBadge';
+import Info from './components/Info';
 import './styles.scss';
 
 export default () => (
@@ -10,26 +11,10 @@ export default () => (
       <img
         src="https://d3e02gns9oqhhr.cloudfront.net/assets/expose_v2/thumbnail.5908c4916a1ca6.30802316.jpg"
         alt="Thumb"
+        width="100%"
+        height="auto"
       />
     </div>
-    <div styleName="info">
-      <div styleName="body">
-        <h2>Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa Casa</h2>
-        <p>35764 Sinn / Fleisbach</p>
-      </div>
-      <div styleName="footer">
-        <div styleName="price">
-          <h2>1.954 €</h2>
-        </div>
-        <div styleName="size">
-          <div styleName="bedrooms">
-            <span>3 Zimmer</span>
-          </div>
-          <div styleName="metres">
-            <span>ab 35 m²</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Info />
   </div>
 );
