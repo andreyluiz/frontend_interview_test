780b599434b53e779643b749707471b6970c6c42
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-07-07 09:24:43 -0300

    Rename component to component.jsx and create an index.js container.

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { fetchAds } from '../../state';
+import App from './component';
+
+// TODO Use reselect.
+const mapStateToProps = state => ({
+  ads: state.ads,
+  isLoading: state.isLoading,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, { fetchAds })(App);
