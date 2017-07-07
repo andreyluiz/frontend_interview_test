e3faea6ce2a0d4aa1697ccc7523c910c0574eb3a
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-07-07 03:59:27 -0300

    Create a basic state slice.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 // @flow
 import React from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { render } from 'react-dom';
 import App from './components/App';
+import { reducer } from './state';
 import './index.scss';
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = (window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store: Object = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk),
+));
 
 const element = document.getElementById('app');
 
-render(<App />, element);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  element);
---
+++ src/state/index.js
@@ -0,0 +1,73 @@
+// @flow
+import { createDuck } from 'redux-duck';
+import axios from 'axios';
+
+/**
+ * I use redux-duck in all my React + Redux projects. This simple library
+ * provides a good way to standardise the way Redux states slices are created
+ * and managed. Check it out below.
+ */
+const duck = createDuck('advertisements');
+
+// Some action types to be dispatched in the request to the API
+const ADS_REQUEST = duck.defineType('ADS_REQUEST');
+const ADS_SUCCESS = duck.defineType('ADS_SUCCESS');
+const ADS_ERROR = duck.defineType('ADS_ERROR');
+
+// The actions from the action types. They may or may not pass a payload attribute.
+const adsRequest = duck.createAction(ADS_REQUEST);
+const adsSuccess = duck.createAction(ADS_SUCCESS);
+const adsError = duck.createAction(ADS_ERROR);
+
+/**
+ * In a more complex application, this action would not have this form.
+ * My recommendation on this cases is to create a middleware and parse a custom
+ * action for specific API requests and optimizations. And example could be:
+ *
+ * {
+ *   types: [ADS_REQUEST, ADS_SUCCESS, ADS_ERROR],
+ *   method: 'get',
+ *   url: '/advertisements',
+ *   onSuccess: resolve
+ * }
+ *
+ * With this shape the actions become easier to test and in a good standard form.
+ *
+ * For the sake of simplicity I'll keep the old fashion async redux action.
+ */
+export const fetchAds = () =>
+  (dispatch: Function) => {
+    // Request set isLoading flag to true
+    dispatch(adsRequest());
+    return axios.get('https://api.mcmakler.de/v1/advertisements')
+      .then(({ data }) => {
+        // Success set isLoading back to false and receives the data.
+        dispatch(adsSuccess(data));
+      })
+      .catch((err) => {
+        // Error set isLoading back to false and receives the error.
+        dispatch(adsError(err.message));
+      });
+  };
+
+const INITIAL_STATE = {
+  isLoading: false,
+  ads: [],
+};
+
+// Redux Duck defines the reducer the Flux way. As an object.
+export const reducer = duck.createReducer({
+  [ADS_REQUEST]: state => ({
+    ...state,
+    isLoading: true,
+  }),
+  [ADS_SUCCESS]: (state, { payload }) => ({
+    ...state,
+    isLoading: false,
+    ads: payload,
+  }),
+  [ADS_ERROR]: state => ({
+    ...state,
+    isLoading: false,
+  }),
+}, INITIAL_STATE);
